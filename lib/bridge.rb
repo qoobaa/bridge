d6801cd0188d42d9a52faa41b6c231506b837dc5
Jakub Kuźma <qoobaa+github@gmail.com>
2010-02-18 22:53:52 +0100

    changed deal number to deal id

--- lib/bridge.rb
+++ lib/bridge.rb
@@ -1,33 +1,33 @@
 module Bridge
 
-  # Total number of possible deals in bridge
+  # Number of possible deals in bridge
   DEALS = 53_644_737_765_488_792_839_237_440_000
 
-  # Array with cards in the bridge deck (from two to ace, four suits)
+  # Array with cards in the bridge deck (AKQJT98765432, four suits)
   DECK = %w(S H D C).inject([]) do |d, s|
     d += %w(A K Q J T 9 8 7 6 5 4 3 2).map { |c| c + s }
   end
 
-  # Converts given number to deal (hash)
-  def self.number_to_deal(number)
+  # Converts given id to deal (hash)
+  def self.id_to_deal(id)
     deal = { :n => [], :e => [], :s => [], :w => [] }
     k = DEALS
     DECK.each_with_index do |card, i|
       x = k * (13 - deal[:n].size) / (52 - i)
-      if number < x
+      if id < x
         deal[:n] << card
       else
-        number -= x
+        id -= x
         x = k * (13 - deal[:e].size) / (52 - i)
-        if number < x
+        if id < x
           deal[:e] << card
         else
-          number -= x
+          id -= x
           x = k * (13 - deal[:s].size) / (52 - i)
-          if number < x
+          if id < x
             deal[:s] << card
           else
-            number -= x
+            id -= x
             x = k * (13 - deal[:w].size) / (52 - i)
             deal[:w] << card
           end
@@ -38,20 +38,20 @@ module Bridge
     deal
   end
 
-  # Converts given deal (hash) to number
-  def self.deal_to_number(d)
-    k = DEALS; number = 0
+  # Converts given deal (hash) to id
+  def self.deal_to_id(d)
+    k = DEALS; id = 0
     deal = { :n => d[:n].dup, :e => d[:e].dup, :s => d[:s].dup, :w => d[:w].dup }
     DECK.each_with_index do |card, i|
       x = k * deal[:n].size / (52 - i)
       unless deal[:n].delete(card)
-        number += x
+        id += x
         x = k * deal[:e].size / (52 - i)
         unless deal[:e].delete(card)
-          number += x
+          id += x
           x = k * deal[:s].size / (52 - i)
           unless deal[:s].delete(card)
-            number += x
+            id += x
             x = k * deal[:w].size / (52 - i)
             deal[:w].delete(card)
           end
@@ -59,21 +59,21 @@ module Bridge
       end
       k = x
     end
-    number
+    id
   end
 
-  # Returns a random deal number
-  def self.random_deal_number
+  # Returns a random deal id
+  def self.random_deal_id
     rand(DEALS)
   end
 
   # Returns a random deal
   def self.random_deal
-    number_to_deal(random_deal_number)
+    id_to_deal(random_deal_id)
   end
 
-  # Checks if the given number is valid deal number
-  def self.deal_number?(n)
+  # Checks if the given number is valid deal id
+  def self.deal_id?(n)
     (0..DEALS - 1).include?(n)
   end
 
--- test/test_bridge.rb
+++ test/test_bridge.rb
@@ -2,35 +2,35 @@ require 'helper'
 
 class TestBridge < Test::Unit::TestCase
   test "first deal conversion" do
-    number = 0
-    deal = Bridge.number_to_deal(number)
+    id = 0
+    deal = Bridge.id_to_deal(id)
     assert Bridge.deal?(deal)
     assert_equal %w(AS KS QS JS TS 9S 8S 7S 6S 5S 4S 3S 2S), deal[:n]
     assert_equal %w(AH KH QH JH TH 9H 8H 7H 6H 5H 4H 3H 2H), deal[:e]
     assert_equal %w(AD KD QD JD TD 9D 8D 7D 6D 5D 4D 3D 2D), deal[:s]
     assert_equal %w(AC KC QC JC TC 9C 8C 7C 6C 5C 4C 3C 2C), deal[:w]
-    assert_equal number, Bridge.deal_to_number(deal)
+    assert_equal id, Bridge.deal_to_id(deal)
   end
 
   test "last deal conversion" do
-    number = Bridge::DEALS - 1
-    deal = Bridge.number_to_deal(number)
+    id = Bridge::DEALS - 1
+    deal = Bridge.id_to_deal(id)
     assert Bridge.deal?(deal)
     assert_equal %w(AC KC QC JC TC 9C 8C 7C 6C 5C 4C 3C 2C), deal[:n]
     assert_equal %w(AD KD QD JD TD 9D 8D 7D 6D 5D 4D 3D 2D), deal[:e]
     assert_equal %w(AH KH QH JH TH 9H 8H 7H 6H 5H 4H 3H 2H), deal[:s]
     assert_equal %w(AS KS QS JS TS 9S 8S 7S 6S 5S 4S 3S 2S), deal[:w]
-    assert_equal number, Bridge.deal_to_number(deal)
+    assert_equal id, Bridge.deal_to_id(deal)
   end
 
   test "deal no 1 000 000 000" do
-    number = 1_000_000_000
-    deal = Bridge.number_to_deal(number)
+    id = 1_000_000_000
+    deal = Bridge.id_to_deal(id)
     assert Bridge.deal?(deal)
-    assert_equal number, Bridge.deal_to_number(deal)
+    assert_equal id, Bridge.deal_to_id(deal)
   end
 
-  test "sample deal to number conversion" do
+  test "sample deal to id conversion" do
     deal = {
       :n => %w(AS KS QS JS AH KH QH AD KD QD AC KC QC),
       :e => %w(TS 9S 8S 7S 6S 5S 4S 3S 2S JH TH 9H 8H),
@@ -38,8 +38,8 @@ class TestBridge < Test::Unit::TestCase
       :w => %w(4D 3D 2D JC TC 9C 8C 7C 6C 5C 4C 3C 2C)
     }
     assert Bridge.deal?(deal)
-    number = Bridge.deal_to_number(deal)
-    assert_equal deal, Bridge.number_to_deal(number)
+    id = Bridge.deal_to_id(deal)
+    assert_equal deal, Bridge.id_to_deal(id)
   end
 
   test "deal with doubled cards is not valid deal" do
@@ -62,17 +62,17 @@ class TestBridge < Test::Unit::TestCase
     assert_false Bridge.deal?(deal)
   end
 
-  test "negative number is not valid deal_number" do
-    assert_false Bridge.deal_number?(-1)
+  test "negative number is not valid deal id" do
+    assert_false Bridge.deal_id?(-1)
   end
 
-  test "total number of possible deals is not valid deal_number" do
-    assert_false Bridge.deal_number?(Bridge::DEALS)
+  test "number of possible bridge deals is not valid deal id" do
+    assert_false Bridge.deal_id?(Bridge::DEALS)
   end
 
-  test "random deal number is valid deal number" do
-    number = Bridge.random_deal_number
-    assert Bridge.deal_number?(number)
+  test "random deal id is valid deal id" do
+    id = Bridge.random_deal_id
+    assert Bridge.deal_id?(id)
   end
 
   test "random deal is valid deal" do
