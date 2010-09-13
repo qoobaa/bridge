ae6a3d1603e4a04e6481d7e0d429c85c75dd544d
Jakub Kuźma <qoobaa+github@gmail.com>
2010-09-14 00:35:31 +0200

    added max percents stub

--- lib/bridge/points/duplicate.rb
+++ lib/bridge/points/duplicate.rb
@@ -7,9 +7,23 @@ module Bridge::Points
     def max
       @scores.inject({}) do |result, score|
         result.tap do |r|
-          r[score] ||= @scores.inject(-1) { |partial, s| partial += (score <=> s) + 1 }
+          r[score] ||= @scores.inject(-1) { |points, s| points += (score <=> s) + 1 }
         end
       end
     end
+
+    # def max_percents
+    #   max.tap do |result|
+    #     result.each do |score, points|
+    #       result[score] = points * 100.0 / theoretical_max
+    #     end
+    #   end
+    # end
+
+    # protected
+
+    # def theoretical_max
+    #   (@scores.size - 1) * 2
+    # end
   end
 end
