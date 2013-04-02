79ffa601659513e0c8ea1918ae87f1d3d5e31f53
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2013-04-02 17:11:42 +0200

    added bid_allowed? method to auction

--- lib/bridge/auction.rb
+++ lib/bridge/auction.rb
@@ -2,7 +2,7 @@ module Bridge
   class Auction
     attr_reader :bids
 
-    def initialize(bids = [])
+    def initialize(bids)
       @bids = bids.map { |bid| Bid.new(bid.to_s) }
     end
 
@@ -11,16 +11,46 @@ module Bridge
     end
 
     def contract
-      if last_contract_index = bids.rindex(&:contract?)
+      if last_contract_index
         modifier = bids[last_contract_index..-1].select(&:modifier?).last
         bids[last_contract_index].to_s + modifier.to_s
       end
     end
 
     def bid_allowed?(bid)
-      return false if finished?
-
       bid = Bid.new(bid.to_s)
+      return false if finished?
+      case
+      when bid.pass?     then true
+      when bid.contract? then contract_allowed?(bid)
+      when bid.double?   then double_allowed?
+      when bid.redouble? then redouble_allowed?
+      end
+    end
+
+    private
+
+    def last_contract_index
+      return @last_contract_index if defined?(@last_contract_index)
+      @last_contract_index = bids.rindex(&:contract?)
+    end
+
+    def contract_allowed?(bid)
+      last_contract_index ? bids[last_contract_index] < bid : true
+    end
+
+    def double_allowed?
+      return false unless last_contract_index
+      after_contract_bids = bids[(last_contract_index + 1)..-1]
+      after_contract_bids.all?(&:pass?) && !after_contract_bids.one?
+    end
+
+    def redouble_allowed?
+      return false unless last_contract_index
+      return false unless last_double_index = bids.rindex(&:double?)
+      return false if last_double_index < last_contract_index
+      after_double_bids = bids[(last_double_index + 1)..-1]
+      after_double_bids.all?(&:pass?) && !after_double_bids.one?
     end
   end
 end
--- test/auction_test.rb
+++ test/auction_test.rb
@@ -39,5 +39,61 @@ describe Bridge::Auction do
     it "returns false if finished" do
       refute Bridge::Auction.new(["1NT", "PASS", "PASS", "PASS"]).bid_allowed?("PASS")
     end
+
+    it "returns true if PASS" do
+      assert Bridge::Auction.new(["1NT", "PASS", "PASS"]).bid_allowed?("PASS")
+    end
+
+    it "returns false if bid is lower than previous" do
+      refute Bridge::Auction.new(["1NT", "PASS"]).bid_allowed?("1S")
+    end
+
+    it "returns true if bid is higher than previous" do
+      assert Bridge::Auction.new(["1NT", "PASS", "X"]).bid_allowed?("2C")
+    end
+
+    it "returns true if bid is first contract" do
+      assert Bridge::Auction.new(["PASS"]).bid_allowed?("2C")
+    end
+
+    it "returns true if bid is double on previous contract" do
+      assert Bridge::Auction.new(["1C"]).bid_allowed?("X")
+    end
+
+    it "returns true if bid is double on previous 2 bids contract" do
+      assert Bridge::Auction.new(["1C", "PASS", "PASS"]).bid_allowed?("X")
+    end
+
+    it "returns false if bid is double on partner contract" do
+      refute Bridge::Auction.new(["1C", "PASS"]).bid_allowed?("X")
+    end
+
+    it "returns false if bid is double on PASS" do
+      refute Bridge::Auction.new(["PASS"]).bid_allowed?("X")
+    end
+
+    it "returns false if bid is double on doubled contract" do
+      refute Bridge::Auction.new(["1C", "X", "PASS"]).bid_allowed?("X")
+    end
+
+    it "returns true if bid is redouble on doubled previous contract" do
+      assert Bridge::Auction.new(["1C", "X"]).bid_allowed?("XX")
+    end
+
+    it "returns true if bid is redouble on 2 bids previous dobuled contract" do
+      assert Bridge::Auction.new(["1C", "X", "PASS", "PASS"]).bid_allowed?("XX")
+    end
+
+    it "returns false if bid is redouble on partners double" do
+      refute Bridge::Auction.new(["1C", "X", "PASS"]).bid_allowed?("XX")
+    end
+
+    it "returns false if bid is redouble on not doubled contract" do
+      refute Bridge::Auction.new(["1C", "PASS", "PASS"]).bid_allowed?("XX")
+    end
+
+    it "returns false if bid is redouble on redoubled contract" do
+      refute Bridge::Auction.new(["1C", "X", "XX", "PASS"]).bid_allowed?("XX")
+    end
   end
 end
