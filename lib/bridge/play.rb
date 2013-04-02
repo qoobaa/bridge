10cfc07af4330771c3cc0235f8e4e64777b25614
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2013-04-02 22:35:56 +0200

    added Play class

---
+++ lib/bridge/play.rb
@@ -0,0 +1,62 @@
+module Bridge
+  class Play
+    attr_reader :deal_id, :contract, :cards
+
+    def initialize(deal_id, contract, cards)
+      @deal_id = deal_id
+      @contract = contract
+      @cards = cards.map { |card| Card.new(card.to_s) }
+    end
+
+    def finished?
+      cards.length == 52
+    end
+
+    def declarer
+      contract[-1]
+    end
+
+    def trump
+      contract[1] if Bridge.trump?(contract[1])
+    end
+
+    def card_allowed?(card)
+      card = Card.new(card.to_s)
+      case
+      when finished?, cards.include?(card), !deal[next_direction].include?(card) then false
+      when tricks.none?, tricks.last.complete? then true
+      else (deal[next_direction] - cards).map(&:suit).uniq.include?(last_lead.suit) ? card.suit == last_lead.suit : true
+      end
+    end
+
+    def directions
+      @directions ||= tricks.map { |trick| trick.cards }.flatten.map { |card| deal.owner(card) }
+    end
+
+    def next_direction
+      case
+      when tricks.none? then Bridge.next_direction(declarer)
+      when tricks.last.complete? then directions.last
+      else Bridge.next_direction(directions.last)
+      end
+    end
+
+    def deal
+      @deal ||= Deal.from_id(deal_id)
+    end
+
+    def tricks
+      @tricks ||= begin
+        tricks = []
+        cards.each_slice(4) { |trick| tricks << Trick.new(*trick) }
+        tricks
+      end
+    end
+
+    private
+
+    def last_lead
+      @last_lead ||= tricks.last.cards.first
+    end
+  end
+end
---
+++ test/play_test.rb
@@ -0,0 +1,82 @@
+require "helper"
+
+describe Bridge::Play do
+  it "returns deal from deal_id" do
+    play = Bridge::Play.new(0, "7SN", [])
+    assert_equal Bridge::Deal, play.deal.class
+  end
+
+  it "returns declarer" do
+    play = Bridge::Play.new(0, "7SN", [])
+    assert_equal "N", play.declarer
+  end
+
+  it "returns trump" do
+    play = Bridge::Play.new(0, "7SN", [])
+    assert_equal "S", play.trump
+
+    play = Bridge::Play.new(0, "7NTN", [])
+    assert_nil play.trump
+  end
+
+  it "returns tricks" do
+    play = Bridge::Play.new(0, "7SN", ["HA", "D2", "C2", "S2", "SA"])
+    assert_equal ["HA", "D2", "C2", "S2"], play.tricks.first.cards.map(&:to_s)
+    assert_equal ["SA"], play.tricks.last.cards.map(&:to_s)
+  end
+
+  it "returns directions of played cards" do
+    play = Bridge::Play.new(0, "7SN", ["HA", "D2", "C2", "S2", "SA"])
+    assert_equal ["E", "S", "W", "N", "N"], play.directions
+  end
+
+  describe "#next_direction" do
+    it "returns direction next to dealer if first lead" do
+      play = Bridge::Play.new(0, "7SN", [])
+      assert_equal "E", play.next_direction
+    end
+
+    it "returns direction of last trick winner when lead" do
+      play = Bridge::Play.new(0, "7SN", ["HA", "D2", "C2", "S2"])
+      assert_equal "N", play.next_direction
+    end
+
+    it "returns direction next to previous if trick not complete" do
+      play = Bridge::Play.new(0, "7SN", ["HA", "D2", "C2"])
+      assert_equal "N", play.next_direction
+    end
+  end
+
+  describe "#card_allowed?" do
+    it "returns true if card is proper" do
+      play = Bridge::Play.new(0, "7SN", ["HA", "D2", "C2"])
+      assert play.card_allowed?("S2")
+    end
+
+    it "returns false if card already played" do
+      play = Bridge::Play.new(0, "7SN", ["HA", "D2", "C2", "S2"])
+      refute play.card_allowed?("S2")
+    end
+
+    it "returns false if card not in hand of next player" do
+      play = Bridge::Play.new(0, "7SN", ["HA", "D2", "C2"])
+      refute play.card_allowed?("CA")
+    end
+
+    it "returns true for other suit if no more cards in first card suit" do
+      play = Bridge::Play.new(636839108127179982824423290, "1SN", ["C5", "CA", "C4", "C3", "C8"])
+      assert play.card_allowed?("S6")
+    end
+
+    it "returns false if card suit other than firsts card in trick" do
+      play = Bridge::Play.new(636839108127179982824423290, "1SN", ["HQ"])
+      refute play.card_allowed?("DT")
+    end
+  end
+end
+
+# deal id: 636839108127179982824423290
+# "N" => ["SA", "SK", "SQ", "S8", "S6", "HK", "H7", "H6", "H4", "DK", "DQ", "DJ", "C3"]
+# "E" => ["S5", "S4", "S3", "HA", "HQ", "HJ", "H9", "D5", "D4", "CK", "CJ", "C9", "C5"]
+# "S" => ["ST", "S7", "S2", "HT", "H8", "H2", "DT", "D8", "D3", "CA", "CT", "C6", "C2"]
+# "W" => ["SJ", "S9", "H5", "H3", "DA", "D9", "D7", "D6", "D2", "CQ", "C8", "C7", "C4"]
